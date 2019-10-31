762babb5c82340eaf921ebf0f58a370e0f10dbda
Mihnea Joita <joitamihnea1999@gmail.com>
2019-10-31 18:24:41 +0000

    Homework done

--- TODO-RO.md
+++ TODO-RO.md
@@ -7,7 +7,7 @@
  - dacă parametrii nu sunt array-uri se va arunca o excepție (`InvalidType`)
 
 # Completați următoarele cerințe
- - `distance` returnează rezultatul corect la pentru 2 array-uri (20%)
+ - `distance` returnează rezultatul corect pentru 2 array-uri (20%)
  - `distance` ignoră elementele duplicate (20%)
  - `distance` aruncă `InvalidType` dacă unul dintre parametri nu este un array (20%)
  - `distance` returnează 0 la compararea a 2 array-uri vide (20%)
--- main/app.js
+++ main/app.js
@@ -1,5 +1,51 @@
 function distance(first, second){
+	if(Array.isArray(first) && Array.isArray(second))
+	{
+		if(first.length!=0 && second.length!=0)
+		{	
+			var vect = [];
+			
+			for (var i = 0; i < first.length; i++) 
+			{
+    			vect[first[i]] = true;
+    		}
 
+    		for (var i = 0; i < second.length; i++) 
+    		{
+    			if (vect[second[i]])
+        		{
+            		delete vect[second[i]];
+        		} 
+        		else 
+        		{
+        			vect[second[i]] = true;
+        		}
+    		}
+    	
+			if((first.every(function(i){ return typeof i === "string" }) && second.every(function(i){ return typeof i !== "string" })) || (first.every(function(i){ return typeof i !== "string" }) && second.every(function(i){ return typeof i === "string" })))
+			{
+				return vect.length;
+			}
+			
+			let vector1 = first.filter( (item) => !second.includes(item) );
+    		
+    		let vector2 = second.filter( (item)=> !first.includes(item)	);
+		
+			let vector3 = vector1.concat(vector2);
+
+			let altvector = Array.from(new Set(vector3));
+
+			return altvector.length;
+		} 
+		else 
+		{
+			return 0;
+		}
+	}
+	else
+	{
+		throw new Error("InvalidType");
+	}
 }
 
 
